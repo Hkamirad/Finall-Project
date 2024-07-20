3188de9a1684525a8bf1788af68f51776b9c2e00
Hkamirad <hamidrezakamranrad@yahoo.com>
2024-07-20 13:59:50 +0330

    add and style search

--- src/components/HeaderandFooter/Header.jsx
+++ src/components/HeaderandFooter/Header.jsx
@@ -9,7 +9,10 @@ import {
 import { useState } from "react";
 
 export default function Header() {
+  const [showSearchInput, setShowSearchInput] = useState(false);
+
   const [dropDownStatus, setDropDownStatus] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
     <>
@@ -21,18 +24,36 @@ export default function Header() {
           <div className="hidden lg:block absolute left-1/2 transform top-4 -translate-x-1/2">
             <NavButtons />
           </div>
-          <div className="hidden lg:flex gap-6">
-            <FontAwesomeIcon
-              icon={faMagnifyingGlass}
-              className="text-white cursor-pointer  duration-100  hover:text-gray-400 hover:scale-110"
-            />
-
+          <div className="hidden lg:flex gap-4 items-center ">
             <FontAwesomeIcon
               icon={faBell}
-              className="text-white cursor-pointer  duration-100  hover:text-gray-400 hover:scale-110"
+              onClick={() => {
+                setIsClicked(!isClicked);
+              }}
+              className={`cursor-pointer duration-100 hover:text-gray-400 hover:scale-110 ${
+                isClicked ? "text-red-600" : "text-white"
+              }`}
             />
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="text-white cursor-pointer transition duration-200 transform hover:text-gray-400 hover:scale-110"
+              onClick={() => {
+                setShowSearchInput(!showSearchInput);
+              }}
+            />
+
+            {showSearchInput && (
+              <input
+                autoFocus
+                onBlur={() => {
+                  setShowSearchInput(!showSearchInput);
+                }}
+                className=" bg-neutral-700  rounded-lg px-1  lg:max-w-44"
+              ></input>
+            )}
           </div>
-          <div className="block lg:hidden ">
+
+          <div className="flex gap-3 items-center lg:hidden  ">
             <FontAwesomeIcon
               icon={faBars}
               onClick={() => {
@@ -43,18 +64,36 @@ export default function Header() {
               }}
               className="cursor-pointer transition duration-200 transform hover:text-gray-400 hover:scale-110"
             />
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              onClick={() => {
+                setShowSearchInput(!showSearchInput);
+              }}
+              className="text-white cursor-pointer transition duration-200 transform hover:text-gray-400 hover:scale-110"
+            />
+
+            {showSearchInput && (
+              <input
+                autoFocus
+                onBlur={() => {
+                  setShowSearchInput(!showSearchInput);
+                }}
+                className=" bg-neutral-700  rounded-lg text-xs  p-1  max-w-28"
+              ></input>
+            )}
           </div>
           {dropDownStatus && (
             <div className="absolute top-16 right-4 text-center bg-stone-950 border border-stone-700 rounded-lg shadow-lg p-4 z-50">
               <NavButtons />
               <div className="flex flex-col  gap-6 mt-4">
                 <FontAwesomeIcon
-                  icon={faMagnifyingGlass}
-                  className="text-white cursor-pointer transition duration-200 transform hover:text-gray-400 hover:scale-110"
-                />
-                <FontAwesomeIcon
                   icon={faBell}
-                  className="text-white cursor-pointer transition duration-200 transform hover:text-gray-400 hover:scale-110"
+                  onClick={() => {
+                    setIsClicked(!isClicked);
+                  }}
+                  className={`cursor-pointer duration-100 hover:text-gray-400 hover:scale-110 ${
+                    isClicked ? "text-red-600" : "text-white"
+                  }`}
                 />
               </div>
             </div>
