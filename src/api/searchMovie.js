c6d2a0e7af4873f0d2db0d02ebd3bb0f18f8fa99
Hkamirad <hamidrezakamranrad@yahoo.com>
2024-07-21 20:32:41 +0330

    connect search to API

--- src/api/searchMovie.js
+++ src/api/searchMovie.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getSerachedMovie = (name = "inception") => {
-  let url = `https://moviesapi.codingfront.dev/api/v1/movies?q={${name}}`;
+  let url = `https://moviesapi.codingfront.dev/api/v1/movies?q=${name}`;
 
   return axios.get(url);
 };
