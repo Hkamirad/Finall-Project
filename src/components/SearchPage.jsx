e07a19033dd6ecc8a2f658dffbdf41413b7eb625
Hkamirad <hamidrezakamranrad@yahoo.com>
2024-07-22 14:25:56 +0330

    edit search page

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -3,25 +3,26 @@ import FreeTrial from "./Freetrial";
 export default function SearchPage({ movies }) {
   return (
     <>
-      <div className="my-20 border border-stone-700 rounded-lg p-6 text-white mb-4 relative">
+      <div className="my-20 border border-stone-700 flex flex-col justify-center rounded-lg p-6 text-white mb-4 relative">
         <div className="absolute bg-red-700 rounded-lg -top-4 left-5 text-center p-2 pt-1 text-sm">
           {" "}
           Search results
         </div>
         {movies.length > 0 ? (
-          <div className="grid grid-cols-2 mt-7 md:grid-cols-3 lg:grid-cols-4 gap-3 2xl:gap-5">
+          <div className="flex flex-wrap justify-center  mt-7 gap-7">
             {movies.map((movie) => (
-              <MovieCard
-                key={movie.id}
-                title={movie.title}
-                poster={movie.poster}
-                id={movie.id}
-              />
+              <div key={movie.id} className="max-w-80">
+                <MovieCard
+                  title={movie.title}
+                  poster={movie.poster}
+                  id={movie.id}
+                />
+              </div>
             ))}
           </div>
         ) : (
           <p className="text-white my-6">
-            Sorry, the movie you have searched for, does not exist! 
+            Sorry, the movie you have searched for, does not exist!
           </p>
         )}
       </div>
